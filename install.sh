110ce9a23b5904656c545196d30b129866ee03d2
bk2dcradle <ankitsultana@gmail.com>
2015-06-23 02:30:57 +0530

    Update

--- install.sh
+++ install.sh
@@ -1,19 +1,10 @@
-# Download and install
-#		1. requests
-			#	git clone git://github.com/kennethreitz/requests.git
-			# python setup.py install
-#		2. terminal-notifier
-			# brew install terminal-notifier
-#	Set home_dir
+# Download and install requests (module for python)
+# If you know your way around git, Install it yourself.
+# Else run this in terminal using instructions in README.md
+
+cd ~/Sub-Downloader
 
 touch logs.txt
-brew help > logs.txt
-if [ -s logs.txt ]; then
-	echo "File there"
-else
-	echo "File not there"
-	# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
 
 # If requests not installed... install it
 python -c "import requests" &> logs.txt
@@ -21,22 +12,22 @@ python -c "import requests" &> logs.txt
 if [ $? -eq 0 ]; then
 	echo "Requests is installed" >> logs.txt
 else
-	echo "Requests not installed, Installing" >> logs.txt
-	for i in 1..3; do
-		echo '.'
-		sleep 1
-	done
-	#cd ~
-	#git clone git://github.com/kennethreitz/requests.git
-	#python setup.py install
-fi
-
-# If terminal-notifier not installed... install it
-terminal-notifier -help >> logs.txt
-
-if [ $? -eq 0 ]; then
-	echo "terminal-notifier already installed!" >> logs.txt
-else
-	echo "install terminal" >> logs.txt
-	#brew install terminal-notifier
+	echo "Requests not installed" >> logs.txt
+	echo "You don't have Requests already installed, Do you want to install it?(y/n)"
+	echo "(Necessary for using Sub-Downloader)"
+	read X
+	
+	if [ $X = 'y' ]; then
+		for i in `seq 1 3`; do
+			echo '.'
+			sleep 1
+		done
+		cd ~
+		git clone git://github.com/kennethreitz/requests.git
+		python setup.py install
+		cd ~/Sub-Downloader
+	else
+		echo "You chose to not install requests. Run this script again if you ever want to install it."
+	fi
+	
 fi
