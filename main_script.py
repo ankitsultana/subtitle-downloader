1f75d09bfe6161427aaa101cb68e413c6d146fc8
bk2dcradle <ankitsultana@gmail.com>
2015-09-14 16:59:54 +0530

    Removed requests

--- main_script.py
+++ main_script.py
@@ -48,24 +48,24 @@ if __name__ == '__main__':
     base_url = 'http://api.thesubdb.com/'
 
     # Use meta_extract module... Part of this bundle
-        logger.info('Extracting Meta Data from File Name')
-        try:
-            dir_name = meta_extract.get_meta(sys.argv[1], 'dir')
-            file_name = meta_extract.get_meta(sys.argv[1], 'file')
-            exten = meta_extract.get_meta(sys.argv[1], 'ext')
-            logger.info("File Name: " + file_name)
-            logger.info("Directory: " + dir_name)
-            logger.info("Extension: " + exten)
-            logger.info('Extracted Meta Data')
-        except Exception, e:
-            logger.exception(e)
+    logger.info('Extracting Meta Data from File Name')
+    try:
+        dir_name = meta_extract.get_meta(sys.argv[1], 'dir')
+        file_name = meta_extract.get_meta(sys.argv[1], 'file')
+        exten = meta_extract.get_meta(sys.argv[1], 'ext')
+        logger.info("File Name: " + file_name)
+        logger.info("Directory: " + dir_name)
+        logger.info("Extension: " + exten)
+        logger.info('Extracted Meta Data')
+    except Exception, e:
+        logger.exception(e)
 
     # Want subs in the same directory as the video files
-        try:
+    try:
         os.chdir(dir_name)
-        except Exception, e:
-            logger.error('Cannot Change Directory')
-            logger.exception(e)
+    except Exception, e:
+        logger.error('Cannot Change Directory')
+        logger.exception(e)
 
     # Unique hash created using the algorithm provided by thesubdb 
     hash_name = str(hash_video.get_hash(file_name+exten))
@@ -73,9 +73,10 @@ if __name__ == '__main__':
     # Change User-Agent
     headers = {'User-Agent': 'SubDB/1.0 (Sub-Downloader/1.0; http://www.github.com/bk2dcradle/Sub-Downloader)'}
 
-        full_url = base_url+'?action=download&hash='+hash_name+'&language=en'
-        status_code = -100
+    full_url = base_url+'?action=download&hash='+hash_name+'&language=en'
+    status_code = '404'
     # Send a get request
+    try:
         if PY_VERSION == 3:
             req = urllib.request.Request(full_url, None, headers)
             response = urllib.request.urlopen(req)
@@ -84,17 +85,19 @@ if __name__ == '__main__':
             req = urllib2.Request(full_url, '', headers)
             response = urllib2.urlopen(req)
             status_code = response.getcode()
+    except Exception, e:
+        logger.error("Error occured while trying to fetch subtitles")
 
-        logger.info(status_code)
+    logger.info(status_code)
 
     if str(status_code) == '200':
-            logger.info('Subtitles Fetched')
-            content = response.read()
-            sub_name = open(file_name+'.srt', 'wb')
-            sub_name.write(content.encode('ascii', 'ignore'))
-            sub_name.close()
+        logger.info('Subtitles Fetched')
+        content = response.read()
+        sub_name = open(file_name+'.srt', 'wb')
+        sub_name.write(content.encode('ascii', 'ignore'))
+        sub_name.close()
     elif str(status_code) == '404':
-            logger.error('Subtitles not available in Database')
+        logger.error('Subtitles not available in Database')
     else:
-            logger.error('Bad Request!')
+        logger.error('Bad Request!')
         logger.info('============================')
