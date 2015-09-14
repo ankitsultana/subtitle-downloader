bb050f2e2a991667fca4d3fe9e12fa6f131eb2e3
bk2dcradle <ankitsultana@gmail.com>
2015-09-14 16:52:54 +0530

    MAJOR: Removed requests

--- main_script.py
+++ main_script.py
@@ -12,23 +12,18 @@ handler.setFormatter(formatter)
 
 logger.addHandler(handler)
 
-try:
-    import sys
-except ImportError, e:
-    logger.error('Cannot import sys')
-    logger.exception(e)
+import sys
 
 logger.info(sys.version)
 
 logger.info('Trying Imports')
 
-try:
-    import sys, os, re
-    logger.info('Imported sys, os and re')
-except ImportError, e:
-    logger.error('Cannot Import sys, os or re')
+PY_VERSION = sys.version_info[0]
 
-sys.dont_write_bytecode = True	# Who likes pyc files
+import os
+logger.info('Imported os')
+
+sys.dont_write_bytecode = True  # Who likes pyc files
 
 # Import custom modules
 try:
@@ -37,19 +32,22 @@ try:
 except ImportError, e:
     logger.error('Cannot Import Custom modules')
 
-# Import requests... may not be present already
 try:
-    import requests
-    logger.info('Imported requests')
-except ImportError, e:
-    logger.error('Cannot Import Requests')
+    logger.info("Trying to import urllib")
+    if PY_VERSION == 2:
+        import urllib2
+        logger.info("PY_VERSION == 2: Importing urllib2: ")
+    elif PY_VERSION == 3:
+        import urllib.request
+        logger.info("PY_VERSION == 3: Importing urllib.request")
+except Exception, e:
+    logger.error("Cannot Import urllib")
     logger.exception(e)
-    logger.error(sys.path)
 
 if __name__ == '__main__':
-	base_url = 'http://api.thesubdb.com/'
+    base_url = 'http://api.thesubdb.com/'
 
-	# Use meta_extract module... Part of this bundle
+    # Use meta_extract module... Part of this bundle
         logger.info('Extracting Meta Data from File Name')
         try:
             dir_name = meta_extract.get_meta(sys.argv[1], 'dir')
@@ -62,33 +60,41 @@ if __name__ == '__main__':
         except Exception, e:
             logger.exception(e)
 
-	# Want subs in the same directory as the video files
+    # Want subs in the same directory as the video files
         try:
-	    os.chdir(dir_name)
+        os.chdir(dir_name)
         except Exception, e:
             logger.error('Cannot Change Directory')
             logger.exception(e)
 
-	# Unique hash created using the algorithm provided by thesubdb 
-	hash_name = str(hash_video.get_hash(file_name+exten))
+    # Unique hash created using the algorithm provided by thesubdb 
+    hash_name = str(hash_video.get_hash(file_name+exten))
 
-	# Change User-Agent
-	headers = {'User-Agent': 'SubDB/1.0 (Sub-Downloader/1.0; http://www.github.com/bk2dcradle/Sub-Downloader)'}
+    # Change User-Agent
+    headers = {'User-Agent': 'SubDB/1.0 (Sub-Downloader/1.0; http://www.github.com/bk2dcradle/Sub-Downloader)'}
 
-	# Send a get request using requests
-	resp = requests.get(base_url+'?action=download&hash='+hash_name+'&language=en', headers=headers)
+        full_url = base_url+'?action=download&hash='+hash_name+'&language=en'
+        status_code = -100
+    # Send a get request
+        if PY_VERSION == 3:
+            req = urllib.request.Request(full_url, None, headers)
+            response = urllib.request.urlopen(req)
+            status_code = response.getcode()
+        elif PY_VERSION == 2:
+            req = urllib2.Request(full_url, '', headers)
+            response = urllib2.urlopen(req)
+            status_code = response.getcode()
 
-        logger.info(resp.status_code)
+        logger.info(status_code)
 
-	if str(resp.status_code) == '200':
+    if str(status_code) == '200':
             logger.info('Subtitles Fetched')
-            content = resp.text
+            content = response.read()
             sub_name = open(file_name+'.srt', 'wb')
             sub_name.write(content.encode('ascii', 'ignore'))
             sub_name.close()
-	elif str(resp.status_code) == '404':
+    elif str(status_code) == '404':
             logger.error('Subtitles not available in Database')
-	else:
+    else:
             logger.error('Bad Request!')
-
-        logger.info('\n')
+        logger.info('============================')
