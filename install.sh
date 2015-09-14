d9c8873e45a74b5ca55d0a5f86aebb8de128c37e
bk2dcradle <ankitsultana@gmail.com>
2015-09-14 17:15:37 +0530

    Removed install script

--- install.sh
+++
@@ -1,5 +0,0 @@
-cd ~/Sub-Downloader
-
-echo "Moving Get Subtitles.workflow"
-echo "Moving Get Subtitles.workflow to ~/Libary/Services/"
-mv ~/Sub-Downloader/Get\ Subtitles.workflow ~/Library/Services/
