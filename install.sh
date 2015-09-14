c360b71e78d2c44c12f877f8aa90bfd5798d8632
bk2dcradle <ankitsultana@gmail.com>
2015-09-14 17:05:29 +0530

    Removed requests installation

--- install.sh
+++ install.sh
@@ -1,40 +1,5 @@
-# Download and install requests (module for python)
-# If you know your way around git, Install it yourself.
-# 	Here is the url: https://github.com/kennethreitz/requests.git
-# Else run this in terminal using instructions in README.md
-
 cd ~/Sub-Downloader
 
-touch logs.txt
-
-# If requests not installed... install it
-python -c "import requests" &> logs.txt
-
-if [ $? -eq 0 ]; then
-	echo "Requests is installed" >> logs.txt
-	echo "You Have Requests already installed"
-else
-	echo "Requests not installed" >> logs.txt
-	echo "You don't have Requests already installed, Do you want to install it?(y/n)"
-	echo "(Necessary for using Sub-Downloader)"
-	read X
-	
-	if [ $X = 'y' ]; then
-		for i in `seq 1 3`; do
-			echo '.'
-			sleep 1
-		done
-		cd ~
-		git clone git://github.com/kennethreitz/requests.git
-		cd ~/requests
-		sudo python setup.py install
-		cd ~/Sub-Downloader
-		echo "Requests installed Successfully" >> logs.txt
-	else
-		echo "You chose to not install requests. Run this script again if you ever want to install it."
-	fi
-fi
-
-echo "Moving Get Subtitles.workflow" >> logs.txt
+echo "Moving Get Subtitles.workflow"
 echo "Moving Get Subtitles.workflow to ~/Libary/Services/"
 mv ~/Sub-Downloader/Get\ Subtitles.workflow ~/Library/Services/
