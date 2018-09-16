48c56048840e5aa67760743183ca5103a3b81733
ankitsultana <ankitsultana@gmail.com>
2018-09-16 20:58:32 +0530

    use osascript for notifications instead of shitty library

--- lib/notify.py
+++ lib/notify.py
@@ -1,19 +1,10 @@
-import Foundation, objc, AppKit
-
-NSUserNotification = objc.lookUpClass('NSUserNotification')
-NSUserNotificationCenter = objc.lookUpClass('NSUserNotificationCenter')
+from subprocess import call
 
 def notify(title, subtitle, info_text, delay=0, sound=False, userInfo={}):
-    notification = NSUserNotification.alloc().init()
-    notification.setTitle_(title)
-    notification.setSubtitle_(subtitle)
-    notification.setInformativeText_(info_text)
-    notification.setUserInfo_(userInfo)
-    if sound:
-        notification.setSoundName_("NSUserNotificationDefaultSoundName")
-    notification.setDeliveryDate_(Foundation.NSDate.dateWithTimeInterval_sinceDate_(delay, Foundation.NSDate.date()))
-    NSUserNotificationCenter.defaultUserNotificationCenter().scheduleNotification_(notification)
-
+    call(["osascript", "-e", "display notification \"" + info_text +
+        "\" with title \"" + title +
+        "\" subtitle \"" + subtitle +
+        "\" sound name \"frog\""])
 
 if __name__ == '__main__':
     notify("Test message", "Subtitle", "This message should appear instantly, with a sound", delay=1000, sound=True)
